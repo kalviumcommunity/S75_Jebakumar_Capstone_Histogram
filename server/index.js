b151ad0dd9c86f5d973db9d264fd566b35119319
Jebakumar <jebakumar.6208@gmail.com>
2025-06-15 22:15:49 +0530

    GET API for core models implemented

--- server/index.js
+++ server/index.js
@@ -10,6 +10,16 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+import userRoutes from './routes/user.routes.js';
+import placeRoutes from './routes/place.routes.js';
+import storyRoutes from './routes/story.routes.js';
+import savedRoutes from './routes/saved.routes.js';
+
+app.use('/api/users', userRoutes);
+app.use('/api/places', placeRoutes);
+app.use('/api/stories', storyRoutes);
+app.use('/api/saved-places', savedRoutes);
+
 connectDB();
 
 app.get("/", (req, res) => {
